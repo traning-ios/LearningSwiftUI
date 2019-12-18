0d75a68b8944ddadc088977cb193ed3d28da77dd
le-xuan-quynh <le-xuan-quynh@goodcycle.net>
2019-12-18 18:32:13 +0900

    add man hinh chao

--- 2-ManhinhChao/2-ManhinhChao/Subview.swift
+++ 2-ManhinhChao/2-ManhinhChao/Subview.swift
@@ -9,14 +9,21 @@
 import SwiftUI
 
 struct Subview: View {
+
     var imageString: String
 
-    var body: some View {        Image(imageString).resizable().aspectRatio(contentMode: .fill).clipped()
+    var body: some View {
+        Image(imageString)
+        .resizable()
+        .aspectRatio(contentMode: .fill)
+        .clipped()
     }
 }
 
+#if DEBUG
 struct Subview_Previews: PreviewProvider {
     static var previews: some View {
         Subview(imageString: "meditating")
     }
 }
+#endif
