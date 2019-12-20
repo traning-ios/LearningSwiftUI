a226df24b507b92f5193d9e6dadcc681c73216a3
le-xuan-quynh <le-xuan-quynh@goodcycle.net>
2019-12-20 09:25:52 +0900

    add new project

---
+++ 6-Nhatky/6-Nhatky/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  6-Nhatky
+//
+//  Created by Le Xuan Quynh on 2019/12/20.
+//  Copyright © 2019 Le Xuan Quynh. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
