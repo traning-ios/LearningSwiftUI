062cee70325d73a120d050b90e63b65ddd0134f5
le-xuan-quynh <le-xuan-quynh@goodcycle.net>
2019-12-23 17:39:20 +0900

    add new redux project

---
+++ 10-UseRedux/10-UseRedux/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  10-UseRedux
+//
+//  Created by Le Xuan Quynh on 2019/12/23.
+//  Copyright © 2019 Le Xuan Quynh. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
