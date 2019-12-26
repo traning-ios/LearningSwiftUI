e717a0450be58469a3cbc305baed1fe72394c6a8
le-xuan-quynh <le-xuan-quynh@goodcycle.net>
2019-12-26 11:38:03 +0900

    add dark mode project

---
+++ 12-CustomDarkMode/12-CustomDarkMode/Theme/UserDefaultsManager.swift
@@ -0,0 +1,27 @@
+//
+//  UserDefaultsManager.swift
+//  12-CustomDarkMode
+//
+//  Created by Le Xuan Quynh on 2019/12/26.
+//  Copyright © 2019 Le Xuan Quynh. All rights reserved.
+//
+
+import Foundation
+
+let userDefaults = UserDefaultsManager()
+
+class UserDefaultsManager: NSObject {
+
+fileprivate let defaults = UserDefaults.standard
+    let selectedTheme = "SelectedTheme"
+    
+    //updating
+    func updateObject(for key: String, with data: Any?) {
+      defaults.set(data, forKey: key)
+      defaults.synchronize()
+    }
+
+    func currentIntObjectState(for key: String) -> Int? {
+      return defaults.integer(forKey: key)
+    }
+}
