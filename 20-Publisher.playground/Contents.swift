ca0e0d2e3eab0ebad28f83ec859fbdb3ea068aaa
le-xuan-quynh <le-xuan-quynh@goodcycle.net>
2020-02-18 15:09:26 +0900

    add samples project

---
+++ 20-Publisher.playground/Contents.swift
@@ -0,0 +1,54 @@
+import UIKit
+import Combine
+import Foundation
+
+//var subscriptions = Set<AnyCancellable>()
+
+public func example(of description: String, action: () -> Void) {
+    print("\n example of: ", description, "---")
+    action()
+}
+
+/*
+example(of: "Custom Subscriber") {
+  // 1
+    let publisher = ["A", "B", "C", "D"].publisher
+
+  // 2
+  final class IntSubscriber: Subscriber {
+    // 3
+    typealias Input = String
+    typealias Failure = Never
+
+    // 4
+    func receive(subscription: Subscription) {
+      subscription.request(.max(3))
+    }
+
+    // 5
+    func receive(_ input: String) -> Subscribers.Demand {
+      print("Received value", input)
+      return .none
+    }
+
+    // 6
+    func receive(completion: Subscribers.Completion<Never>) {
+      print("Received completion", completion)
+    }
+  }
+
+    let subscriber = IntSubscriber()
+
+    publisher.subscribe(subscriber)
+}
+
+*/
+
+
+example(of: "Future") {
+  func futureIncrement(
+    integer: Int,
+    afterDelay delay: TimeInterval) -> Future<Int, Never> {
+
+  }
+}
