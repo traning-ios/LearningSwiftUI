dae50dd50b6526b7c6e8b201f0013fd31caa2dfb
le-xuan-quynh <le-xuan-quynh@goodcycle.net>
2020-03-05 14:02:21 +0900

    init combine login project

---
+++ 24-CombineLoginProject/24-CombineLoginProjectTests/_4_CombineLoginProjectTests.swift
@@ -0,0 +1,34 @@
+//
+//  _4_CombineLoginProjectTests.swift
+//  24-CombineLoginProjectTests
+//
+//  Created by Le Xuan Quynh on 2020/03/05.
+//  Copyright © 2020 Le Xuan Quynh. All rights reserved.
+//
+
+import XCTest
+@testable import _4_CombineLoginProject
+
+class _4_CombineLoginProjectTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
